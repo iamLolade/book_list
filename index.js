30f89ee63d2ebc53116c5551c9a320340093ae83
iamLolade <adeniyiotemade@gmail.com>
2021-12-24 18:07:15 +0100

    Implemented add function with DOM

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const btns = document.querySelectorAll("#book-list .delete");
+const list = document.querySelector("#book-list ul");
 
-Array.from(btns).forEach(btn => {
-    btn.addEventListener("click", (e) => {
+list.addEventListener("click", (e) => {
 
+    if(e.target.className == "delete") {
         const li = e.target.parentElement;
+        list.removeChild(li)
+    }
 
-        li.parentNode.removeChild(li);
+})
 
-    })
+//Add book
+const addForm = document.forms["add-book"];
+
+addForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const value = addForm.querySelector('input[type="text"]').value;
+
+    //Create element
+    const li = document.createElement("li");
+    const bookName = document.createElement("span");
+    const deleteBtn = document.createElement("span");
+
+    //add content
+    bookName.textContent = value;
+    deleteBtn.textContent = "-";
+
+    //append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li)
 })
