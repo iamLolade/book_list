e66ddac0e53a4695524af5fc17676d020d6fab89
iamLolade <adeniyiotemade@gmail.com>
2021-12-28 10:05:18 +0100

    Implemented search function with DOM

--- index.js
+++ index.js
@@ -26,8 +26,47 @@ addForm.addEventListener("submit", (e) => {
     bookName.textContent = value;
     deleteBtn.textContent = "-";
 
+    //add classes
+    bookName.classList.add("name");
+    deleteBtn.classList.add("delete")
+
+
     //append to document
     li.appendChild(bookName);
     li.appendChild(deleteBtn);
     list.appendChild(li)
 })
+
+//hide books
+const hideBox = document.querySelector("#hide");
+
+hideBox.addEventListener("change", (e) => {
+    
+    if(hideBox.checked) {
+        list.style.display = "none"
+    } else {
+        list.style.display = "initial"
+    }
+
+})
+
+//filter books
+const searchBar = document.forms["search-books"].querySelector("input");
+
+searchBar.addEventListener("keyup", (e) => {
+    const term = e.target.value.toLowerCase();
+
+    const books = list.getElementsByTagName("li")
+    Array.from(books).forEach(book => {
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = "block";
+        } else {
+            book.style.display = "none";
+        }
+    })
+})
+
+//tabbed content
+const tabs = document.querySelector(".tabs");
+const panels = document.querySelectorAll(".panel");
