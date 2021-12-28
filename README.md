62f949a9597e3729d16a889e1a9ffa2d3b2b0e87
iamLolade <adeniyiotemade@gmail.com>
2021-12-28 12:43:45 +0100

    Created README file

---
+++ README.md
@@ -0,0 +1,5 @@
+# Book List Demo Application
+
+## It utilizes DOM manipulation for it's functionalities. Users can add, search and remove books from the list
+
+![Screenshot](/image/screenshot.png)
--- index.js
+++ index.js
@@ -1,72 +1,71 @@
-const list = document.querySelector("#book-list ul");
+document.addEventListener("DOMContentLoaded", () => {
 
-list.addEventListener("click", (e) => {
+    const list = document.querySelector("#book-list ul");
 
-    if(e.target.className == "delete") {
-        const li = e.target.parentElement;
-        list.removeChild(li)
-    }
+    list.addEventListener("click", (e) => {
 
-})
-
-//Add book
-const addForm = document.forms["add-book"];
-
-addForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const value = addForm.querySelector('input[type="text"]').value;
-
-    //Create element
-    const li = document.createElement("li");
-    const bookName = document.createElement("span");
-    const deleteBtn = document.createElement("span");
-
-    //add content
-    bookName.textContent = value;
-    deleteBtn.textContent = "-";
-
-    //add classes
-    bookName.classList.add("name");
-    deleteBtn.classList.add("delete")
-
-
-    //append to document
-    li.appendChild(bookName);
-    li.appendChild(deleteBtn);
-    list.appendChild(li)
-})
-
-//hide books
-const hideBox = document.querySelector("#hide");
-
-hideBox.addEventListener("change", (e) => {
-    
-    if(hideBox.checked) {
-        list.style.display = "none"
-    } else {
-        list.style.display = "initial"
-    }
-
-})
-
-//filter books
-const searchBar = document.forms["search-books"].querySelector("input");
-
-searchBar.addEventListener("keyup", (e) => {
-    const term = e.target.value.toLowerCase();
-
-    const books = list.getElementsByTagName("li")
-    Array.from(books).forEach(book => {
-        const title = book.firstElementChild.textContent;
-        if(title.toLowerCase().indexOf(term) != -1) {
-            book.style.display = "block";
-        } else {
-            book.style.display = "none";
+        if(e.target.className == "delete") {
+            const li = e.target.parentElement;
+            list.removeChild(li)
         }
+
+    })
+
+    //Add book
+    const addForm = document.forms["add-book"];
+
+    addForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const value = addForm.querySelector('input[type="text"]').value;
+
+        //Create element
+        const li = document.createElement("li");
+        const bookName = document.createElement("span");
+        const deleteBtn = document.createElement("span");
+
+        //add content
+        bookName.textContent = value;
+        deleteBtn.textContent = "-";
+
+        //add classes
+        bookName.classList.add("name");
+        deleteBtn.classList.add("delete")
+
+
+        //append to document
+        li.appendChild(bookName);
+        li.appendChild(deleteBtn);
+        list.appendChild(li)
+    })
+
+    //hide books
+    const hideBox = document.querySelector("#hide");
+
+    hideBox.addEventListener("change", (e) => {
+        
+        if(hideBox.checked) {
+            list.style.display = "none"
+        } else {
+            list.style.display = "initial"
+        }
+
+    })
+
+    //filter books
+    const searchBar = document.forms["search-books"].querySelector("input");
+
+    searchBar.addEventListener("keyup", (e) => {
+        const term = e.target.value.toLowerCase();
+
+        const books = list.getElementsByTagName("li")
+        Array.from(books).forEach(book => {
+            const title = book.firstElementChild.textContent;
+            if(title.toLowerCase().indexOf(term) != -1) {
+                book.style.display = "block";
+            } else {
+                book.style.display = "none";
+            }
+        })
     })
 })
-
-//tabbed content
-const tabs = document.querySelector(".tabs");
-const panels = document.querySelectorAll(".panel");
